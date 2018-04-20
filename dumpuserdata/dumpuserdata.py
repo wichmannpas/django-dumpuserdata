bda469282d1739c5c28309695d2e8ecea1d4eb45
Pascal Wichmann <git@pa-w.de>
2018-04-20 17:33:30 +0200

    Follow both relational directions, but only reverse related transitively.
    This is probably not sufficient for most use cases. Instead, some kind
    of model/field annotation is required to decide which relations to
    follow transitively and which not. Otherwise, all reachable model
    objects would be dumped.

--- dumpuserdata/dumpuserdata.py
+++ dumpuserdata/dumpuserdata.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Tuple
 
 from django.apps import apps
 from django.db import models
@@ -8,6 +9,7 @@ def dump_data(obj) -> dict:
     """Dump all data that is associated to obj. """
     data = defaultdict(list)
 
+    added = set()
     processed = set()
     new = set()
     new.add(obj)
@@ -15,13 +17,33 @@ def dump_data(obj) -> dict:
     while new:
         current = new.pop()
         processed.add(current)
+        added.add(current)
         data['.'.join((
             current._meta.model.__module__,
             current._meta.model.__name__
         ))].append(current)
 
 
-        for obj in related_objects(current):
+        related, reverse_related = related_objects(current)
+        # TODO: it can probably not generically be determined whether
+        # transitive relations should be followed, some kind of
+        # model/field annotations are required for this.
+        for obj in related:
+            """
+            we want to add related objects, but not
+            transitive relations.
+            """
+            if obj not in added:
+                data['.'.join((
+                    obj._meta.model.__module__,
+                    obj._meta.model.__name__
+                ))].append(obj)
+                added.add(obj)
+        for obj in reverse_related:
+            """
+            we want to follow reverse related objects
+            transitively (i.e., following the directed edges)
+            """
             if obj in processed:
                 continue
             new.add(obj)
@@ -29,13 +51,26 @@ def dump_data(obj) -> dict:
     return dict(data)
 
 
-def related_objects(obj) -> set:
+def related_objects(obj) -> Tuple[set, set]:
     """
-    Get all objects that directly (i.e., without transitivity) reference obj.
+    Get two sets containing
+
+    * all objects that are directly (i.e., without
+        transitivity) referenced by obj
+    * all objects that directly (i.e., without transitivity)
+        reference obj
     """
-    objects = set()
+    related = set()
+    reverse_related = set()
 
     obj_model = obj._meta.model
+
+    for field in obj_model._meta.fields:
+        if not isinstance(field, models.fields.related.RelatedField):
+            continue
+
+        # TODO: this probably results in undesired behaviour for ManyToMany fields
+        related.add(getattr(obj, field.name))
 
     for model in apps.get_models():
         for field in model._meta.fields:
@@ -50,9 +85,9 @@ def related_objects(obj) -> set:
             if obj_model != target_model:
                 continue
 
-            objects |= set(
+            reverse_related |= set(
                 model.objects.filter(**{
                     field.name: obj,
                 }))
 
-    return objects
+    return related, reverse_related
